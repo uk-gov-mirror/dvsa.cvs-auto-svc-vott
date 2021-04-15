e90f66ee8288ff5dfceb08947049e1a9924de3fd
Robert Whitehouse <Robert.Whitehouse@bjss.com>
2021-04-15 11:39:23 +0100

    imported files from cvs-auto-svc repo

---
+++ src/main/java/vott/AccessToken.java
@@ -0,0 +1,26 @@
+package vott;
+
+import java.time.Duration;
+
+public class AccessToken {
+
+    private Duration expiresIn;
+    private String accessToken;
+
+    public Duration getExpiresIn() {
+        return expiresIn;
+    }
+
+    public void setExpiresIn(Duration expiresIn) {
+        this.expiresIn = expiresIn;
+    }
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+}
---
+++ src/main/java/vott/GrantType.java
@@ -0,0 +1,24 @@
+package vott;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public enum GrantType {
+    IMPLICIT {
+        @Override
+        protected Map<String, Object> formParams() {
+            Map<String, Object> formParams = new HashMap<>();
+
+            return formParams;
+        }
+    },
+    CLIENT_CREDENTIALS {
+        @Override
+        protected Map<String, Object> formParams() {
+            Map<String, Object> formParams = new HashMap<>();
+
+            return formParams;
+        }
+    };
+    protected abstract Map<String, Object> formParams();
+}
